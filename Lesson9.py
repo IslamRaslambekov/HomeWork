c4c74d256ebb821c69c4247c11a4305a263e0c16
SaydEmin <rodoslav12345@gmail.com>
2020-07-19 16:18:06 +0300

    Hello

---
+++ Lesson9.py
@@ -0,0 +1,26 @@
+import random
+
+
+class Fool:
+    deck_list = ['6♠', '6♥', '6♣', '6♦', '7♠', '7♥', '7♣', '7♦',
+                 '8♠', '8♥', '8♣', '8♦', '9♠', '9♥', '9♣', '9♦',
+                 '10♠', '10♥', '10♣', '10♦', 'J♠', 'J♥', 'J♣', 'J♦',
+                 'Q♠', 'Q♥', 'Q♣', 'Q♦', 'K♠', 'K♥', 'K♣', 'K♦',
+                 'A♠', 'A♥', 'A♣', 'A♦']
+
+    def mix_deck(self):
+        # Перемешка карт
+        pass
+
+    def on_hand(self):
+        # Выдача карт в начале игры
+        pass
+
+    def get_cards(self):
+        # Получить дополнительную карту
+        pass
+
+
+if __name__ == '__main__':
+    new_party = Fool()
+    print(new_party.suit_hearts)
