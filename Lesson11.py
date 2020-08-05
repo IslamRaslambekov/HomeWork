2f268bb845acdc76df2cfedd81ff155f35f25908
SaydEmin <rodoslav12345@gmail.com>
2020-08-05 21:06:07 +0300

    Commit

--- Lesson11.py
+++ Lesson11.py
@@ -10,26 +10,46 @@ class Point:
         return f'Точка с координатами {self.x} и {self.y}'
 
     def __eq__(self, other):
-        if (self.x == other.x) and (self.y == other.y):
+        if (self.x is other.x) and (self.y is other.y):
             return True
         return False
 
     def __ne__(self, other):
-        if (self.x is not other.x) and (self.y is not other.y):
+        if (self.x is not other.x) or (self.y is not other.y):
             return True
         return False
 
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
 
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
     def __mul__(self, other):
         return Point(self.x * other.x, self.y * other.y)
+
+    def __truediv__(self, other):
+        return Point(self.x / other.x, self.y / other.y)
+
+    def __floordiv__(self, other):
+        return Point(self.x // other.x, self.y // other.y)
+
+    def __mod__(self, other):
+        return Point(self.x % other.x, self.y % other.y)
+
+    def __pow__(self, other):
+        return Point(self.x ** other.x, self.y ** other.y)
 
 
 if __name__ == "__main__":
     pnt1 = Point(1, 10)
-    pnt2 = Point(1, 10)
-    print(pnt1 == pnt2)
-    print(pnt1 != pnt2)
-    print(pnt1 + pnt2)
-    print(pnt1 * pnt2)
+    pnt2 = Point(2, 10)  # Перегрузка:
+    print(pnt1 == pnt2)  # равенства
+    print(pnt1 != pnt2)  # неравенства
+    print(pnt1 + pnt2)  # сложения
+    print(pnt1 - pnt2)  # вычитания
+    print(pnt1 * pnt2)  # умножения
+    print(pnt1 / pnt2)  # деления
+    print(pnt1 // pnt2)  # деления без плавающей точки
+    print(pnt1 % pnt2)  # остатка от деления
+    print(pnt1 ** pnt2)  # возведения в степень
