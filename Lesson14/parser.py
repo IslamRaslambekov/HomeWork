5dd361680f350f5e47ac8ef8cde13f3f86769dcd
IslamRaslambekov <rodoslav12345@gmail.com>
2020-09-01 12:38:10 +0300

    Lesson14

---
+++ Lesson14/parser.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+HOST = 'https://www.kant.ru/'
+URL = 'https://www.kant.ru/catalog/bikes/#/filter:page=1&'
+
+html = requests.get(URL).text
+
+soup = BeautifulSoup(html, 'html.parser')
+containers = soup.find_all('div', class_='kant__catalog__item__content')
+bikes = list()
+for container in containers:
+    link = HOST + container.find('a').get('href')
+    model = container.find('div', {'class': 'kant__catalog__item-title'}).getText()[12:].replace('            ', '')
+    price = container.find('span', {'class': 'kant__price'}).getText(strip=True)
+    if container.find('span', {'class': 'kant__catalog__price--old kant__price'}) is not None:
+        old_price = container.find('span', {'class': 'kant__catalog__price--old kant__price'}).getText(strip=True)
+    else:
+        old_price = None
+
+    bikes.append({
+        'link': link,
+        'model': model,
+        'price': price,
+        'old_price': old_price
+    })
+
+with open('bikes.csv', 'w', newline='', encoding='UTF-8') as file:
+    writer = csv.writer(file, delimiter=';')
+    headers = ['Link', 'Model', 'Price', 'Old price']
+    writer.writerow(headers)
+
+    for bike in bikes:
+        writer.writerow([bike['link'], bike['model'], bike['price'], bike['old_price']])
---
+++ Lesson14/parser_2.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+HOST = 'https://www.velosite.ru'
+URL = 'https://www.velosite.ru/catalog/search/?root_category=1&sort=popularity&sort_type=desc&view=blocks&in_stock=1' \
+      '&brand%5B%5D=273&brand%5B%5D=305&brand%5B%5D=415&gender=&stature= '
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/84.0.4147.135 Safari/537.36 '
+}
+html = requests.get(URL).text
+
+soup = BeautifulSoup(html, 'html.parser')
+containers = soup.find_all('div', class_='card--inner-hover')
+bikes = list()
+
+for container in containers:
+    link = HOST + container.find('div', class_='card__bottom_price--right global-grid-50 grid-parent').find('a').get('href')
+    model = container.find('div', class_='card--inner').find('a', class_='noborder card__info').getText()[11:]
+    price = container.find('div', class_='card__bottom_price').find('span', class_='accent-red').getText().replace('\xa0', ' ')
+    old_price = None
+
+    bikes.append({
+        'link': link,
+        'model': model,
+        'price': price,
+        'old_price': old_price
+    })
+
+with open('bikes.csv', 'a', newline='', encoding='UTF-8') as file:
+    writer = csv.writer(file, delimiter=';')
+
+    for bike in bikes:
+        writer.writerow([bike['link'], bike['model'], bike['price'], bike['old_price']])
