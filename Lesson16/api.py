ef283070d0854560efa42caa2f4fc8fb1b3cf48e
IslamRaslambekov <rodoslav12345@gmail.com>
2020-09-10 13:18:18 +0300

    Lesson16

---
+++ Lesson16/api.py
@@ -0,0 +1,51 @@
+import requests
+from pprint import pprint
+
+
+def find_vacancies(parameters):
+    URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    response = requests.get(URL).json()
+    usd_rate = response['Valute']['USD']['Value']
+    euro_rate = response['Valute']['EUR']['Value']
+
+    URL_HH = 'https://api.hh.ru/vacancies'
+
+    min_salary = list()
+    max_salary = list()
+
+    response = requests.get(URL_HH, params=parameters).json()
+    pages = response['pages']
+
+    for page in range(pages + 1):
+
+        params = {'text': parameters.get('text'),
+                  'only_with_salary': parameters.get('only_with_salary'),
+                  'per_page': parameters.get('per_page'),
+                  'page': page}
+
+        response = requests.get(URL_HH, params=params).json()
+
+        for item in response['items']:
+            salfrom = item['salary']['from']
+            salto = item['salary']['to']
+            salcurr = item['salary']['currency']
+
+            if salcurr == 'RUR':
+                if salfrom is not None:
+                    min_salary.append(salfrom)
+                if salto is not None:
+                    max_salary.append(salto)
+
+            elif salcurr == 'USD':
+                if salfrom is not None:
+                    min_salary.append(int(salfrom * usd_rate))
+                if salto is not None:
+                    max_salary.append(int(salto * usd_rate))
+
+            elif salcurr == 'EUR':
+                if salfrom is not None:
+                    min_salary.append(int(salfrom * euro_rate))
+                if salto is not None:
+                    max_salary.append(int(salto * euro_rate))
+
+    return f'Средняя з/п разработчиков по запросу {parameters["text"]} от {sum(min_salary) // len(min_salary)} до {sum(max_salary) // len(max_salary)} RUR'
---
+++ Lesson16/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from flask import render_template as rt
+from Lesson16.api import find_vacancies as fv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/vacancies')
+def get_vacancies():
+    return rt('vacancies.html')
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    search = request.form['search']
+    if request.form['city']:
+        city = str(request.form['city']).capitalize()
+        params = {'text': f'{search} AND {city}', 'only_with_salary': True, 'per_page': 100}
+    else:
+        params = {'text': search, 'only_with_salary': True, 'per_page': 100}
+
+    data = {}
+    data['response'] = fv(params)
+    return rt('result.html', **data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
