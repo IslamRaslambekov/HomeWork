d27246db8b8a8e5bee3ea4d082c703cbdd6a8383
IslamRaslambekov <rodoslav12345@gmail.com>
2021-10-22 18:04:09 +0300

    Commit

--- Lesson24/RSP/management/commands/update_doctor.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from RSP.models import Doctor
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        doctor = Doctor.objects.filter(id=1).update(name='Alex')
--- Lesson24/RSP/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from RSP import views
-
-urlpatterns = [
-    path('', views.main_page, name='home'),
-    path('doctors/', views.Doctors.as_view(), name='doctors'),
-    path('add_doctors/', views.AddDoctors.as_view(), name='add_doctors')
-]
